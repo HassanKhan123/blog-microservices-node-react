a4284e8496b572371ca6f022e73799a0bc986555
HassanKhan123 <hk2602809@gmail.com>
2022-11-01 20:57:55 +0500

    added: comment moderation service

--- comments/index.js
+++ comments/index.js
@@ -36,8 +36,25 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Comments EVENTS RECEIVED=========", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post(`http://localhost:4005`, {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
